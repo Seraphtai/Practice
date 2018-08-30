0c42535521de8ab39c69d9e908bae31af1f78cc3
Seraphtai <31637882+Seraphtai@users.noreply.github.com>
2018-08-30 11:22:35 +0800

    Animation

---
+++ Animation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour {
+    public Sprite[] sprites;
+    public int fps;
+
+    private float t;
+    private float time;
+    private SpriteRenderer spriteRenderer;
+    private int index;
+    // Use this for initialization
+    void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        t = 1.0f / fps;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time >= t)
+        {
+            time = 0;
+            spriteRenderer.sprite = sprites[index];
+            index++;
+            if (index >= sprites.Length)
+            {
+                index = 0;
+            }
+        }
+    }
+}
